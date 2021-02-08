3bd156d8834395d83cfd449c2216449f0f40d14c
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-02-08 14:58:10 +0200

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const dateOptions = {
 document.getElementById('date').innerHTML = new Date().toLocaleDateString('en-US', dateOptions)
 
 
+const add = document.getElementById('add')
 const list = document.getElementById('list')
 const input = document.getElementById('input')
 
@@ -35,16 +36,15 @@ function addTask(task, id, done, bin) {
 }
 
 
-var id = 1, activeTasks = [] 
-
-
+var id = 0, activeTasks = [] 
 var data = localStorage.getItem('Task')
+
 if (data) {
     activeTasks = JSON.parse(data)
     id = activeTasks.length
     loadList(activeTasks)
 } else {
-    id = 1
+    id = 0
     activeTasks = []
 }
 
@@ -56,29 +56,44 @@ function loadList(arr) {
 
 
 document.querySelector('.clear').addEventListener('click', () => {
-    localStorage.clear()
-    location.reload()
-})
-
-document.addEventListener('keyup', (event) => {
-    let task = input.value
-    if (event.key === 'Enter') {
-        if (task) {
-            addTask(task, id, false, false)
-            activeTasks.push({
-                task: task,
-                id: id,
-                done: false,
-                bin: false
-            })
-            localStorage.setItem('Task', JSON.stringify(activeTasks))
-            id++
+    if (document.getElementsByClassName('item').length > 0) {
+        if (window.confirm('Task list will be deleted')) {
+            localStorage.clear()
+            location.reload()
         }
-        input.value = ''
-        console.log(activeTasks)
     }
 })
 
+
+add.addEventListener('click', displayTask)
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        displayTask()
+    }
+})
+
+
+function displayTask() {
+    let task = input.value
+    if (task) {
+        addTask(task, id, false, false)
+        activeTasks.push({
+            task: task,
+            id: id,
+            done: false,
+            bin: false
+        })
+        localStorage.setItem('Task', JSON.stringify(activeTasks))
+        id++
+    } else {
+        input.placeholder = '( ͠ಠ ͜ʖ ͠ಠ ) Task can\'t be empty!'
+        setTimeout(() => { 
+            input.placeholder = 'Add Task'
+        }, 2400)
+    }
+    input.value = ''
+    console.log(activeTasks)
+}
 
 function completeTask(element) {
     element.classList.toggle('fa-check-circle')
